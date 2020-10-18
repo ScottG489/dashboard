530f0a75eb876acf7e74cd2130b7a6ecebb7e46f
Scott Giminiani <scottg489@gmail.com>
2020-10-17 21:26:21 -0400

    Set the bucket name properly
    - Fix bug with outputs being named wrong

--- infra/tf/modules/dashboard_website_core/main.tf
+++ infra/tf/modules/dashboard_website_core/main.tf
@@ -4,8 +4,8 @@ resource "aws_route53_zone" "r53_zone" {
 
 module "helpers_s3_website_subdomain" {
   source = "ScottG489/helpers/aws//modules/s3_website_subdomain"
-  version = "0.0.9"
-  name = var.domain_name
+  version = "0.1.0"
+  bucket_name = "${var.subdomain}.${var.domain_name}"
   subdomain = var.subdomain
   route53_zone_id = aws_route53_zone.r53_zone.id
 }
--- infra/tf/modules/dashboard_website_core/outputs.tf
+++ infra/tf/modules/dashboard_website_core/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket" {
-  value = module.helpers_s3_website.bucket_name
+  value = module.helpers_s3_website_subdomain.bucket_name
 }
 
 output "bucket_website_endpoint" {
-  value = module.helpers_s3_website.website_endpoint
+  value = module.helpers_s3_website_subdomain.website_endpoint
 }
 
 output "r53_zone_name" {
