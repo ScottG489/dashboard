f811b2da7ba2d1d685600c1595393defa262718a
Scott Giminiani <scottg489@gmail.com>
2020-10-19 21:38:59 -0400

    Remove no longer needed example component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Example from "./Example";
 import GitHubBuilds from "./GitHubBuilds";
 
 let App = () => {
     return (
         <div className="App container">
             <div className="row justify-content-center">
-                <div className="col-auto">
-                    <pre>
-                        {/*<Example/>*/}
-                    </pre>
-                </div>
                 <div className="col-auto">
                     <GitHubBuilds />
                 </div>
