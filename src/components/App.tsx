403977a5af3d11632f97c9c3729dc3df73b9339b
Scott Giminiani <scottg489@gmail.com>
2021-11-14 22:51:03 -0500

    Don't display mint dashboard card
    I don't think this dashboard is an appropriate place for this card. It
    was a good experiment to play around with runtime secrets, but the point
    of the dashboard is for public information.
    Additionally, the backend job for this isn't even working right now due
    to a bug or two in the underlying mint lib.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,6 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 import GitHubBuilds from "./GitHubBuilds";
-import Mint from "./Mint";
 
 let App = () => {
     return (
@@ -13,9 +12,6 @@ let App = () => {
             <div className="card-columns justify-content-center">
                 <div className="card">
                     <GitHubBuilds />
-                </div>
-                <div className="card">
-                    <Mint />
                 </div>
             </div>
         </div>
