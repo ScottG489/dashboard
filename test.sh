fa381410874955f088033085adda97a8bf31abe2
Scott Giminiani <scottg489@gmail.com>
2022-07-01 00:04:57 -0400

    Use a unique tag for local build image when running test script
    I wanted to be able to run local test builds in parallel. I think it may
    actually have been fine to do it before this since once one image builds
    it starts immediately. But I wanted to be safe and create unique names
    for the images.

--- test.sh
+++ test.sh
@@ -23,8 +23,9 @@ sudo chown -R "$(whoami)":"$(whoami)" -- * .*
 find . -name '*terraform.tfstate*' -exec rm {} \;
 find . -name '.terraform' -type d -prune -exec rm -rf {} \;
 
-docker build infra/build -t app-test && \
+LOCAL_IMAGE_TAG="dashboard-build-test-$(uuidgen | cut -c -8)"
+docker build infra/build -t $LOCAL_IMAGE_TAG && \
 docker run -it \
   --runtime=sysbox-runc \
   --volume "$PWD:/home/build-user/build/dashboard" \
-  app-test "$JSON_BODY"
+  $LOCAL_IMAGE_TAG "$JSON_BODY"
