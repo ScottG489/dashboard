39514a9d1766a812c9549ea2c473970c62165e85
Scott Giminiani <scottg489@gmail.com>
2022-07-03 17:17:02 -0400

    Add dark theme
    Note that this seems to only darken the area around the card rather than
    the whole page like we want.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 import GitHubBuilds from "./GitHubBuilds";
 import Container from "@mui/material/Container";
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 let App = () => {
     return (
-        <Container>
-            <GitHubBuilds/>
-        </Container>
+        <ThemeProvider theme={darkTheme}>
+            <Container>
+                <GitHubBuilds/>
+            </Container>
+        </ThemeProvider>
     )
 }
 
