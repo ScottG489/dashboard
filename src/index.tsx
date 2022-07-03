63fd7876e9d881fa567b68f0d151a7fc27e5dc90
Scott Giminiani <scottg489@gmail.com>
2022-07-03 14:20:46 -0400

    Use react 18 method of rendering
    ReactDOM.render was deprecated

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
+// TODO: I believe this is necessary until Typescript types come out for react 18
+if (!rootElement) throw new Error('Failed to find the root element')
+const root = createRoot(rootElement)
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
