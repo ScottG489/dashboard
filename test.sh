78b61b38c96b5905e3d1848cf8410814b33d052f
Scott Giminiani <scottg489@gmail.com>
2025-11-16 10:14:10 -0500

    Fix issues with the build user on Ubuntu 24
    Now that the docker image has an unprivileged user we don't need to
    create and use our own anymore. This was also causing issues using
    test.sh locally. We instead just use the already provided ubuntu user.

--- test.sh
+++ test.sh
@@ -24,9 +24,9 @@ sudo chown -R "$(whoami)":"$(whoami)" .
 find . -name '*terraform.tfstate*' -exec rm {} \;
 find . -name '.terraform' -type d -prune -exec rm -rf {} \;
 
+BUILD_USER=ubuntu
 LOCAL_IMAGE_TAG="dashboard-build-test-$(uuidgen | cut -c -8)"
 docker build infra/build -t $LOCAL_IMAGE_TAG && \
 docker run -it \
-  --runtime=sysbox-runc \
-  --volume "$PWD:/home/build-user/build/dashboard" \
+  --volume "$PWD:/home/$BUILD_USER/build/dashboard" \
   $LOCAL_IMAGE_TAG "$JSON_BODY"
