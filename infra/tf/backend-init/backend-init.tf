7f5f8f115b40159edf8bba561755a4e589e712a3
Scott Giminiani <scottg489@gmail.com>
2025-11-16 23:12:30 -0500

    Migrate terraform config to use aws_s3_bucket_versioning
    This is necessary as of v4 of the provider. More info:
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-4-upgrade#migrating-to-aws_s3_bucket_versioning

--- infra/tf/backend-init/backend-init.tf
+++ infra/tf/backend-init/backend-init.tf
@@ -5,8 +5,12 @@ provider "aws" {
 resource "aws_s3_bucket" "backend_bucket" {
   bucket = var.tfstate_backend_bucket_name
   force_destroy = true
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "backend_bucket" {
+  bucket = aws_s3_bucket.backend_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
