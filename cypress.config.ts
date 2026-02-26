4dd8a3cb7b5abdaa9246269dd11a9e6123d9939b
ScottG489 <scottg489@gmail.com>
2026-02-26 12:13:08 -0500

    Remove build container after run
    Keeping these containers around indefinitely uses up too much disk
    space. We already have the logs in github and we rarely if ever need the
    container around for any purpose like debugging.

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   defaultCommandTimeout: 30000,
   env: {
-    backendUrl: "https://api.conjob.io/job/run?image=scottg489/gh-repo-build-status-job:latest",
+    backendUrl: "https://api.conjob.io/job/run?image=scottg489/gh-repo-build-status-job:latest&remove=true",
   },
   e2e: {
     setupNodeEvents(on, config) {},
